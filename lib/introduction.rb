79c5e855bba89ad41de82de527ccabf39ea26210
Samuel Ferebee <sam.ferebee@gmail.com>
2020-12-22 20:55:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,8 +11,13 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def introduction_with_language_optional(name, language =  "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 # introduction("Sam")
-introduction_with_language("Sam", "Ruby")
+# introduction_with_language("Sam", "Ruby")
+introduction_with_language_optional("Sam")
 
 
 
